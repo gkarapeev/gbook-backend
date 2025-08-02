909f90152d7cb9205a8cde6b3010b6a1e3f73c48
Gesh <nqma@ti.kaja>
2025-08-02 23:31:33 +0300

    feat: db loc in env

--- db/db-init.go
+++ db/db-init.go
@@ -4,11 +4,18 @@ import (
 	"database/sql"
 	"log"
 
+	"os"
+
+	"github.com/joho/godotenv"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func InitDB() *sql.DB {
-	db, err := sql.Open("sqlite3", "./db/data.db")
+	godotenv.Load()
+	location := os.Getenv("DB_LOCATION")
+
+	db, err := sql.Open("sqlite3", location)
 	if err != nil {
 		log.Fatal(err)
 	}
